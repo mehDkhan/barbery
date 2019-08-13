9b778e985853abf36fc9a9958b2c88ad8cbcbaa5
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-13 21:47:24 +0430

    define user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    location = models.CharField(max_length=50,blank=True,null=True)
+    bio = models.CharField(max_length=280,blank=True,null=True)
+    date_of_birth = models.DateField(null=True)
+
+    def __str__(self):
+        return self.username
