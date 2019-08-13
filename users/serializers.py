6002b32ac05e20e96c4da4bd98b54959319226ea
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2019-08-13 21:47:44 +0430

    define user serializer

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
